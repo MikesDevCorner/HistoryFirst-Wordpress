af73bee1330138d2033b40965e875a0c0ce43150
Christoph <christoph.scharwitzl@students.fh-hagenberg.at>
2019-08-28 21:48:36 +0200

    modified quote

--- wp-content/themes/wp_first/layout/quote.php
+++ wp-content/themes/wp_first/layout/quote.php
@@ -1,7 +1,9 @@
 <div class="row">
     <div class="col-md-10 col-xl-9">
         <blockquote>
-          <?php the_sub_field("quote"); ?>
+          <div class="blockquote__inner">
+            <?php the_sub_field("quote"); ?>
+          </div>
           <h4>&mdash; <?php echo str_replace(array('<p>','</p>'),'',get_sub_field("author")); ?></h4>
         </blockquote>
     </div>
